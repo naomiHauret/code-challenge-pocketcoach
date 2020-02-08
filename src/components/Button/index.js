0e2db08fa966637ddbd81c48f5994e9a42aebeb5
Naomi Hauret <hauretnaomi@gmail.com>
2020-02-08 15:46:26 +0100

    prettify code

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,35 +3,21 @@ import React from "react"
 import styles from "./index.module.css"
 
 const Button = (props) => {
-
   const { children, handleClick, disabled } = props
 
   return (
-
     <button
-
       className={`transition font-sans duration-150 ease-in-out ${styles.button} ${
-
         disabled
-
           ? "opacity-25 pointer-events-none cursor-not-allowed "
-
           : "bg-white text-blue-600 focus:bg-blue-800 focus:border-blue-800 focus:text-white  hover:bg-blue-600 hover:border-blue-600 hover:text-white border-solid border-blue-600 opacity-100 cursor-pointer"
-
       } rounded-full tracking-wide  py-2 min-w-10 border-solid font-bold `}
-
       disabled={disabled}
-
       onClick={handleClick}
-
     >
-
       {children}
-
     </button>
-
   )
-
 }
 
 export default Button
--- src/utils/seed/index.js
+++ src/utils/seed/index.js
@@ -1,27 +1,21 @@
 // Generate data
 
 export function seed(number) {
+  const offers = {}
 
-    const offers = {}
-  
-    const dd = new Date()
-  
-    for (let i = 0; i < number; i++) {
-  
-      offers[i] = {
-  
-        id: i,
-  
-        date: new Date(dd.setMinutes(dd.getMinutes() + i * number)),
-  
-        amount: Math.random() * number,
-  
-        bid: 9.5 + Math.random(),
-  
-      }
-  
+  const dd = new Date()
+
+  for (let i = 0; i < number; i++) {
+    offers[i] = {
+      id: i,
+
+      date: new Date(dd.setMinutes(dd.getMinutes() + i * number)),
+
+      amount: Math.random() * number,
+
+      bid: 9.5 + Math.random(),
     }
-  
-    return offers
-  
   }
+
+  return offers
+}
